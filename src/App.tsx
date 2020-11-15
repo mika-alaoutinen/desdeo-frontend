5c08a0b9f2700226bab6bd7bf0d178a00c0c5bd1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-11-15 17:36:08 +0200

    Add stub HomeView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import HomeView from './views/HomeView'
+
 const App: React.FC = () => {
   
   return (
     <div className="App">
       <h1>DESDEO frontend</h1>
+      <HomeView />
     </div>
   )
 }
