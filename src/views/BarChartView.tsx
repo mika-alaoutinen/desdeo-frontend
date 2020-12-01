57656db0a48e70c4bdf446fb0557a71e1457e27f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-01 20:32:53 +0200

    Add typed selector for desdeo

--- src/views/BarChartView.tsx
+++ src/views/BarChartView.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
+import BarChart from 'desdeo-d3-barchart'
+import { useSelector } from '../store/desdeo/desdeoTypes'
+
 const BarChartView: React.FC = () => {
+  const data = useSelector(state => state.variables)
+  const size = [500, 500]
+
+  const emitUpdate = (): void => {
+    // Dispatch the update to Redux store
+    console.log('update data')
+  }
+
   return (
     <div>
       <h2>Bar chart page</h2>
-      
+      <BarChart props={{
+          data,
+          size,
+          doUpdate: emitUpdate
+        }}
+      />
     </div>
   )
 }
