0781874f96af2ef05b49ad9198a6a241fd90295b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-03 19:18:50 +0200

    Separate regular actions from thunk actions

--- src/store/desdeo/desdeoActions.ts
+++ src/store/desdeo/desdeoActions.ts
@@ -1,32 +1,5 @@
-import { AppThunk } from '../reduxTypes'
+import { REVERSE_VARIABLES, DesdeoAction } from './desdeoTypes'
 
-import {
-  ADD_PARAMETER, ADD_VARIABLE, RUN_ITERATION, Parameter, Variable
-} from './desdeoTypes'
-
-import desdeo from '../../services/desdeoService'
-
-export const addVariable = (newVar: Variable): AppThunk => async dispatch => {
-  const variable = await desdeo.addVariable(newVar)
-  dispatch({
-    type: ADD_VARIABLE,
-    variable
-  })
-}
-
-export const addParameter = (newParam: Parameter): AppThunk => async dispatch => {
-  const parameter = await desdeo.addParameters(newParam)
-  dispatch({
-    type: ADD_PARAMETER,
-    parameter
-  })
-}
-
-export const runIteration = (): AppThunk => async (dispatch, getState) => {
-  const currentIteration = getState().desdeo
-  const iteration = await desdeo.runIteration(currentIteration)
-  dispatch({
-    type: RUN_ITERATION,
-    iteration
-  })
-}
+export const reverseVariables = (): DesdeoAction => ({
+  type: REVERSE_VARIABLES
+})
