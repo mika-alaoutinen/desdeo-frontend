23098e4cd5a76d21cb7dd9093fb3c166cfd3d847
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-05 12:36:10 +0200

    Add About view

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { barchart, home } from '../../constants'
+import { about, barchart, home } from '../../constants'
 
 const Navigation: React.FC = () => (
   <nav>
     <Link style={{ paddingRight: '0.5em' }} to={home}>
       Home
+    </Link>
+
+    <Link style={{ paddingRight: '0.5em' }} to={about}>
+      About
     </Link>
 
     <Link style={{ paddingRight: '0.5em' }} to={barchart}>
--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import AboutView from '../views/AboutView'
 import BarChartView from '../views/BarChartView'
 import HomeView from '../views/HomeView'
-import { barchart, home } from '../constants'
+import { about, barchart, home } from '../constants'
 
 const Routes: React.FC = () => (
   <Switch>
+    <Route exact path={about} component={AboutView} />
     <Route exact path={barchart} component={BarChartView} />
     <Route exact path={home} component={HomeView} />
   </Switch>
