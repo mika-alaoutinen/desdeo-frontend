61b3e72e2d0fcd7ee77082afed8b5880aafa0a6e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-05 14:47:08 +0200

    WIP navigation

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Drawer, List, ListItem, ListItemText } from '@material-ui/core'
 
 import { about, barchart, home } from '../../constants'
 
-const Navigation: React.FC = () => (
-  <nav>
-    <Link style={{ paddingRight: '0.5em' }} to={home}>
-      Home
-    </Link>
+const Navigation: React.FC = () => {
 
-    <Link style={{ paddingRight: '0.5em' }} to={about}>
-      About
-    </Link>
+  const createListItems = (): JSX.Element[] => {
+    const links = [
+      { text: 'Home', to: home },
+      { text: 'About', to: about },
+      { text: 'Bar chart', to: barchart },
+    ]
 
-    <Link style={{ paddingRight: '0.5em' }} to={barchart}>
-      Bar chart
-    </Link>
-  </nav>
-)
+    return links.map(link => createListItem(link.text, link.to))
+  }
+
+  const createListItem = (text: string, to: string): JSX.Element => (
+    <ListItem
+      key={text}
+      button
+      component={Link}
+      to={to}
+    >
+      <ListItemText primary={text} />
+    </ListItem>
+  )
+  
+  return (
+    <Drawer
+      anchor='left'
+      variant='permanent'
+    >
+      <List>
+        {createListItems()}
+      </List>
+    </Drawer>
+  )
+}
 
 export default Navigation
