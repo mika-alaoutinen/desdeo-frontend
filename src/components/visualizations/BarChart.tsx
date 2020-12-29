44b375803ebb5c085e764c51dac68c14ff521d57
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-12-29 15:28:03 +0200

    Move testdata to store folder

--- src/components/visualizations/BarChart.tsx
+++ src/components/visualizations/BarChart.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from 'react'
 
 import { BarChartComponent } from 'desdeo-components'
-import { testdata } from '../../misc/testdata'
-import { EventHandler } from 'desdeo-components/build/types/eventTypes'
+import { testdata } from '../../store/testdata'
 
 const BarChart: React.FC = () => {
   const [ data, setData ] = useState(testdata)
 
-  const eventHandler: EventHandler = {
-    type: 'USE_STATE',
-    callback: setData
-  }
-  
   return (
     <BarChartComponent
       data={data}
-      eventHandler={eventHandler}
+      eventHandler={{
+        type: 'USE_STATE',
+        callback: setData
+      }}
     />
   )
 }
