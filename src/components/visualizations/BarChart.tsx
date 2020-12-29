2b607f1196bf164926449163e90733985c5d2d88
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2020-12-29 15:32:21 +0200

    Dev (#10)
    Add desdeo-components as dependency and remove desdeo-d3-barchart

---
+++ src/components/visualizations/BarChart.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+import { BarChartComponent } from 'desdeo-components'
+import { testdata } from '../../store/testdata'
+
+const BarChart: React.FC = () => {
+  const [ data, setData ] = useState(testdata)
+
+  return (
+    <BarChartComponent
+      data={data}
+      eventHandler={{
+        type: 'USE_STATE',
+        callback: setData
+      }}
+    />
+  )
+}
+
+export default BarChart
