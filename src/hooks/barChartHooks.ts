074405773cd07dff138a3602358a3cc16bced772
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-06 12:01:50 +0200

    Move useData hook to selectors file

--- src/hooks/barChartHooks.ts
+++ src/hooks/barChartHooks.ts
@@ -1,10 +1,5 @@
-import { Datum } from 'desdeo-components/build/types/dataTypes'
-
 import { useSelector } from '../store/reduxTypes'
 import { Variable } from '../store/desdeo/desdeoTypes'
-
-export const useData = (): Datum[] =>
-  useSelector(state => state.data)
 
 export const useMaxVariable = (): Variable => {
   const variables = useSelector(state => state.desdeo.variables)
