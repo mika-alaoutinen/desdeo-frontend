dd8f9448e7a679b53f461720dcce427e21dba71e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-06 13:26:49 +0200

    Refactor thunk actions to have explicit return types

--- src/store/desdeo/desdeoThunkActions.ts
+++ src/store/desdeo/desdeoThunkActions.ts
@@ -1,32 +1,36 @@
-import { AppThunk } from '../reduxTypes'
+import { Action } from 'redux'
 
+import { AppThunk } from '../reduxTypes'
 import {
   ADD_PARAMETER, ADD_VARIABLE, RUN_ITERATION, Parameter, Variable
 } from './desdeoTypes'
-
 import desdeo from '../../services/desdeoService'
 
-export const addVariable = (newVar: Variable): AppThunk => async dispatch => {
-  const variable = await desdeo.addVariable(newVar)
-  dispatch({
-    type: ADD_VARIABLE,
-    variable
-  })
+
+export const addVariable = (newVar: Variable): AppThunk =>
+  async (dispatch): Promise<Action> => {
+    const variable = await desdeo.addVariable(newVar)
+    return dispatch({
+      type: ADD_VARIABLE,
+      variable
+    })
 }
 
-export const addParameter = (newParam: Parameter): AppThunk => async dispatch => {
-  const parameter = await desdeo.addParameters(newParam)
-  dispatch({
-    type: ADD_PARAMETER,
-    parameter
-  })
+export const addParameter = (newParam: Parameter): AppThunk =>
+  async (dispatch): Promise<Action> => {
+    const parameter = await desdeo.addParameters(newParam)
+    return dispatch({
+      type: ADD_PARAMETER,
+      parameter
+    })
 }
 
-export const runIteration = (): AppThunk => async (dispatch, getState) => {
-  const currentIteration = getState().desdeo
-  const iteration = await desdeo.runIteration(currentIteration)
-  dispatch({
-    type: RUN_ITERATION,
-    iteration
-  })
+export const runIteration = (): AppThunk =>
+  async (dispatch, getState): Promise<Action> => {
+    const currentIteration = getState().desdeo
+    const iteration = await desdeo.runIteration(currentIteration)
+    return dispatch({
+      type: RUN_ITERATION,
+      iteration
+    })
 }
