70427f91028be206e65c057b4a77a1a97393124c
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-06 13:49:04 +0200

    Put charts in a grid

--- src/views/ChartsView.tsx
+++ src/views/ChartsView.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 
 import BarChart from '../components/visualizations/BarChart'
 import ScatterSelection from '../components/visualizations/ScatterSelection'
 
-const ChartsView: React.FC = () => (
-  <div className='ChartsView'>
-    <h2>Charts page</h2>
-    <BarChart />
-    <ScatterSelection />
-  </div>
-)
+const ChartsView: React.FC = () => {
+
+  const createSubHeading = (text: string): JSX.Element =>
+    <h3 style={{ textAlign: 'center' }}>
+      {text}
+    </h3>
+
+  return (
+    <div className='ChartsView'>
+      <h2>Charts page</h2>
+
+      {createSubHeading('Clickable charts')}
+      <Grid container justify='center'>
+        <Grid item>
+          <BarChart />
+        </Grid>
+        <Grid item>
+          <ScatterSelection />
+        </Grid>
+      </Grid>
+
+      {createSubHeading('Selectable charts')}
+      <Grid container justify='center'>
+        <Grid item>
+          <BarChart />
+        </Grid>
+        <Grid item>
+          <ScatterSelection />
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
 
 export default ChartsView
