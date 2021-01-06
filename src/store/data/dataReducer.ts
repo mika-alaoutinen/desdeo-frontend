d1c3e6805c6049c5c4f535bd54e7d56c0e63a4e2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-06 17:58:00 +0200

    Redo dataReducer

--- src/store/data/dataReducer.ts
+++ src/store/data/dataReducer.ts
@@ -1,6 +1,7 @@
 import { Datum } from 'desdeo-components/build/types/dataTypes'
 import {
-  ADD_DATA, SELECT_DATA, SELECT_DATUM, DataAction, DataState
+  ADD_DATA, CLEAR_SELECTED, SELECT_DATA, SELECT_DATUM,
+  DataAction, DataState
 } from './dataTypes'
 
 const data = (state: DataState = [], action: DataAction): DataState => {
@@ -9,27 +10,48 @@ const data = (state: DataState = [], action: DataAction): DataState => {
     case ADD_DATA:
       return state.concat(action.data)
 
+    case CLEAR_SELECTED:
+      return clearSelected(state)
+
     case SELECT_DATA:
-      return editSelectedData(action.data, state)
+      return selectData(action.data, state)
 
     case SELECT_DATUM:
-      return state.map(datum =>
-        datum.id === action.datum.id ? action.datum : datum)
+      return selectDatum(action.datum, state)
 
     default:
       return state
   }
 }
 
-const editSelectedData = (selected: Datum[], data: Datum[]): Datum[] => {
+// Export these for easier testing
+export const clearSelected = (data: Datum[]): Datum[] =>
+  data.map(datum => setSelected(datum, false))
+
+export const selectData = (selected: Datum[], data: Datum[]): Datum[] => {
   const selectedIDs = selected.map(datum => datum.id)
-  return data.map(datum => selectedIDs.includes(datum.id)
-    ? {
-      ...datum,
-      isSelected: true
-    }
-    : datum
-  )
+  return data.map(datum => mapSelected(selectedIDs, datum))
 }
+
+export const selectDatum = (selected: Datum, data: Datum[]): Datum[] =>
+  data.map(datum => datum.id === selected.id
+    ? editSelected(selected)
+    : datum)
+
+// Utility functions
+const editSelected = (datum: Datum): Datum =>
+  datum.isSelected === undefined
+    ? setSelected(datum, true)
+    : setSelected(datum, !datum.isSelected)
+
+const mapSelected = (selectedIDs: string[], datum: Datum): Datum =>
+  selectedIDs.includes(datum.id)
+    ? setSelected(datum, true)
+    : datum
+
+const setSelected = (datum: Datum, isSelected: boolean): Datum => ({
+  ...datum,
+  isSelected
+})
 
 export default data
