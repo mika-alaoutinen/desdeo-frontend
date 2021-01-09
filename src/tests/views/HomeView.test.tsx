8ee46b1df7d0f47b4354b10e1aeea614e4ef3410
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-09 15:46:21 +0200

    Add a couple of tests for HomeView

---
+++ src/tests/views/HomeView.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import HomeView from '../../views/HomeView'
+
+beforeEach(() => {
+  render(<HomeView />).container
+})
+
+describe('HomeView component is rendered', () => {
+  it('component is truthy', () => {
+    expect(HomeView).toBeTruthy()
+  })
+})
+
+describe('View contains a heading', () => {
+  it('has a heading', () => {
+    expect(screen.getByText('Home page')).toBeTruthy()
+  })
+})
