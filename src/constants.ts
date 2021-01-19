e689813cc703c84eb55cea7fc211bf45abbf94c9
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-01-19 19:40:32 +0200

    Add link to components' GitHub repository

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const GITHUB_URL = 'https://github.com/mika-alaoutinen/desdeo-frontend'
+export const GITHUB_URL_COMPONENTS = 'https://github.com/mika-alaoutinen/desdeo-components'
+export const GITHUB_URL_FRONTEND = 'https://github.com/mika-alaoutinen/desdeo-frontend'
