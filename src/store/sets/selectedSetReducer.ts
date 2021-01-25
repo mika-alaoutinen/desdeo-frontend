f174566f88564826a4072fa25bd351761ffb53e0
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2021-01-25 16:55:19 +0200

    Selected set state handling (#18)
    Add temporary selected set state slice to Redux store

---
+++ src/store/sets/selectedSetReducer.ts
@@ -0,0 +1,22 @@
+import { Coordinate } from 'desdeo-components/build/types/dataTypes'
+import { SELECT_SET, SetAction } from './setTypes'
+
+const init: Coordinate = {
+  id: '1',
+  x: 0,
+  y: 0
+}
+
+// Temporary reducer used to display a selected coordinate on the UI
+const selectedSet = (state: Coordinate = init, action: SetAction): Coordinate => {
+  switch (action.type) {
+
+    case SELECT_SET:
+      return action.datum
+
+    default:
+      return state
+  }
+}
+
+export default selectedSet
--- src/views/ChartsView.tsx
+++ src/views/ChartsView.tsx
@@ -4,11 +4,15 @@ import { Grid } from '@material-ui/core'
 import BarChart from '../components/visualizations/bar/BarChart'
 import GroupedBarChart from '../components/visualizations/bar/GroupedBarChart'
 import StackedBarChart from '../components/visualizations/bar/StackedBarChart'
+import ParallelAxes from '../components/visualizations/parallelAxes/ParallelAxes'
 import ScatterChart from '../components/visualizations/scatter/ScatterChart'
 import ScatterSelection from '../components/visualizations/scatter/ScatterSelection'
 import Table from '../components/visualizations/table/Table'
 
+import { useSelectedSet } from '../hooks/selectors'
+
 const ChartsView: React.FC = () => {
+  const { id, label, y } = useSelectedSet()
 
   const createSubHeading = (text: string): JSX.Element =>
     <h3 style={{ textAlign: 'center' }}>
@@ -33,6 +37,11 @@ const ChartsView: React.FC = () => {
     <div className='ChartsView'>
       <h2>Charts page</h2>
 
+      <p>Selected coordinate:</p>
+      <p>ID: {id}</p>
+      <p>Label: {label}</p>
+      <p>Y: {y}</p>
+
       {createSubHeading('Bar charts')}
       {createGrid(<BarChart />, <GroupedBarChart />, <StackedBarChart />)}
 
@@ -41,6 +50,9 @@ const ChartsView: React.FC = () => {
 
       {createSubHeading('Basic table')}
       {createGrid(<Table />)}
+
+      {createSubHeading('Filtering')}
+      {createGrid(<ParallelAxes />)}
     </div>
   )
 }
