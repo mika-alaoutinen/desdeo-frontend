ee227d2d75dd509603dd0269be8c59cd7f7a2ccd
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-06 16:50:27 +0200

    Clean up code

--- src/data/dataTransformations.ts
+++ src/data/dataTransformations.ts
@@ -1,32 +1,26 @@
-import { Coordinate, CoordinateSet, DataSet } from './dataTypes'
+import { CoordinateSet, DataSet } from './dataTypes'
 
-// Corresponds to groupedByAlternatives
 const createAlternativeSets = (dataset: DataSet): CoordinateSet[] =>
   dataset.map(({ data, label }) => {
-    const coordinates = data.map((value, i) => createCoordinate(label, i + 1, value))
+    const coordinates = data.map((value, i) => ({
+      id: createId(label, i + 1),
+      x: i + 1,
+      y: value
+    }))
     return { data: coordinates }
   })
 
-// Corresponds to groupedByCriteria
 const createCriteriaSets = (dataset: DataSet): CoordinateSet[] =>
-  dataset[0].data.map((_, columnIndex) => {
-    const coordinates = dataset.map((row, rowIndex) => {
-      return {
-        id: createId(row.label, columnIndex + 1),
-        x: rowIndex + 1,
-        y: row.data[columnIndex]
-      }
-    })
+  dataset[0].data.map((_, colIndex) => {
+    const coordinates = dataset.map(({ data, label }, rowIndex) => ({
+      id: createId(label, colIndex + 1),
+      x: rowIndex + 1,
+      y: data[colIndex]
+    }))
     return { data: coordinates }
   })
 
 // Utility functions
-const createCoordinate = (label: string, x: number, y: number): Coordinate => ({
-  id: createId(label, x),
-  x,
-  y
-})
-
 const createId = (label: string, n: number): string => {
   const hyphenated = label.toLowerCase().replace(/ /g, '-')
   return `${hyphenated}-${n}`
