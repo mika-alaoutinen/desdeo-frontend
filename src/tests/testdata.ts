884bc40d767d8fa6002d08932ee23b16ef133114
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-06 13:30:01 +0200

    Add dataTypes

--- src/tests/testdata.ts
+++ src/tests/testdata.ts
@@ -1,8 +1,32 @@
 import {
-  Coordinate, CoordinateSet, ParallelAxesData
-} from 'desdeo-components/build/types/dataTypes'
+  Coordinate, CoordinateSet, DataSet, ParallelAxesData
+} from 'data/dataTypes'
 
-export const coordinates: Coordinate[] = [
+const optimizationData: DataSet = [
+  {
+    label: 'WQ Fishery',
+    data: [ 6.042483, 5.758127, 6.287081, 6.134672, 5.610188, 5.231501, 6.34, 6.291364, 5.407513, 6.019503 ]
+  },
+  {
+    label: 'WQ City',
+    data: [ 3.17527, 3.410843, 3.207926, 2.98383, 2.910456, 3.248641, 2.962557, 3.346416, 3.130143, 3.350959 ]
+  },
+  {
+    label: 'ROI',
+    data: [ 6.090291, 6.887735, 2.992514, 5.507545, 7.082375, 7.352708, 0.321111, 2.847139, 7.254194, 6.195485 ]
+  },
+  {
+    label: 'City Tax',
+    data: [ 2.444406, 8.989781, 2.758216, 0.581456, 0.216794, 3.951754, 0.377181, 5.67065, 2.057297, 6.173211 ]
+  },
+  {
+    label: 'Plant Resources',
+    data: [ 0.248895, 0.346752, 0.326688, 0.259547, 0.126336, 0.295807, 0.35, 0.328574, 0.228541, 0.327455 ]
+  }
+]
+
+// Old test data
+const coordinates: Coordinate[] = [
   { id: 'a', x: 10, y: 20, label: 'A', isSelected: true },
   { id: 'b', x: 20, y: 40, label: 'B', isSelected: false },
   { id: 'c', x: 30, y: 70, label: 'C' },
@@ -10,7 +34,7 @@ export const coordinates: Coordinate[] = [
   { id: 'e', x: 50, y: 50, label: 'E' },
 ]
 
-export const coordinateSets: CoordinateSet[] = [
+const coordinateSets: CoordinateSet[] = [
   {
     data: [
       { id: 'G', x: 1, y: 80, label: 'C', isSelected: true },
@@ -39,7 +63,7 @@ export const coordinateSets: CoordinateSet[] = [
   }
 ]
 
-export const filterData: ParallelAxesData[] = [
+const filterData: ParallelAxesData[] = [
   {
     name: 'Adrien',
     attributes: [
@@ -65,3 +89,7 @@ export const filterData: ParallelAxesData[] = [
     ]
   },
 ]
+
+export {
+  coordinates, coordinateSets, filterData, optimizationData
+}
