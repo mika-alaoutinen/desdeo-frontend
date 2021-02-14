b1664534058df7625f2c996e32f6483f0a560f30
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-14 16:44:39 +0200

    Uninstall axios and redux-thunk

--- src/store/reduxTypes.ts
+++ src/store/reduxTypes.ts
@@ -1,20 +1,11 @@
 import { createSelectorHook } from 'react-redux'
-import { Action } from 'redux'
-import { ThunkAction } from 'redux-thunk'
+
 import { CoordinateAction } from './coordinates/coordinateTypes'
 import { DatasetAction } from './dataset/datasetTypes'
-
 import { reducer } from './store'
-
-// Selector for root level state:
-export const useSelector = createSelectorHook<RootState, RootAction>()
-
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->
 
 type RootAction = CoordinateAction | DatasetAction
 type RootState = ReturnType<typeof reducer>
+
+// Selector for root level state:
+export const useSelector = createSelectorHook<RootState, RootAction>()
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
 
 import coordinates from './coordinates/coordinateReducer'
 import dataset from './dataset/datasetReducer'
@@ -14,7 +13,7 @@ export const reducer = combineReducers({
 
 export const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools()
 )
 
 export default store
