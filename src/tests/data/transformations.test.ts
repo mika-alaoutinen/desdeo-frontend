41423810dd936cf1e170653174848a5dccbb6de8
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-02-19 14:20:05 +0200

    Add input transformation

---
+++ src/tests/data/transformations.test.ts
@@ -0,0 +1,33 @@
+import { createDataSet } from 'data/transformations'
+import { DataSet, InputDataSet } from 'misc/dataTypes'
+
+describe('createDataSet maps input into a DataSet with labels and Values', () => {
+  const input: InputDataSet = [
+    { label: 'Set A', data: [1, 2] },
+    { label: 'Set B', data: [3, 4] },
+  ]
+
+  it('maps data as expected', () => {
+    const expected: DataSet = [
+      {
+        label: 'Set A',
+        data: [
+          { id: 'set-a-0', isSelected: false, value: 1 },
+          { id: 'set-a-1', isSelected: false, value: 2 },
+        ],
+      },
+      {
+        label: 'Set B',
+        data: [
+          { id: 'set-b-0', isSelected: false, value: 3 },
+          { id: 'set-b-1', isSelected: false, value: 4 },
+        ],
+      },
+    ]
+    expect(createDataSet(input)).toEqual(expected)
+  })
+
+  it('handles an empty input', () => {
+    expect(createDataSet([])).toEqual([])
+  })
+})
